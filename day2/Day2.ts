996a76845dbc1bd14e73358223d2ce6c06242332
elmoren <elmore.nathan@gmail.com>
2023-12-06 17:15:21 -0700

    Day2 Part2 finished

--- day2/Day2.ts
+++ day2/Day2.ts
@@ -14,6 +14,14 @@ interface Game {
     grabs: Grab[]
 }
 
+function emptyGrab(): Grab {
+    return {
+        red: 0,
+        green: 0,
+        blue: 0
+    };
+}
+
 function parseGame(line: string): Game {
     let strings = line.split(":");
     let grabs = strings[1].split(";");
@@ -22,14 +30,11 @@ function parseGame(line: string): Game {
         gameId: Number(strings[0].split(" ")[1]),
         grabs: grabs.map(g => {
 
-            let grab: Grab = {
-                red: 0,
-                green: 0,
-                blue: 0
-            };
+            let grab: Grab = emptyGrab();
 
             g.split(",").forEach(e => {
                     let pair = e.trim().split(" ");
+                    // @ts-ignore
                     grab[pair[1]] = Number(pair[0]);
                 }
             );
@@ -39,9 +44,24 @@ function parseGame(line: string): Game {
     }
 }
 
-let result = data
-    .map(l => parseGame(l))
+let games: Game[] = data.map(l => parseGame(l));
+
+let part1 = games
     .filter(e => e.grabs.every(value => value.red <= 12 && value.green <= 13 && value.blue <= 14))
     .reduce((acc, cur) => acc + cur.gameId, 0);
 
-console.log(result);
+console.log(part1);
+
+let part2 = games
+    .map(g => {
+        return g.grabs.reduce((acc, g) => {
+            return {
+                red: acc.red > g.red ? acc.red : g.red,
+                green: acc.green > g.green ? acc.green : g.green,
+                blue: acc.blue > g.blue ? acc.blue : g.blue
+            } as Grab;
+        }, emptyGrab())
+    })
+    .reduce((acc, g) => acc + (g.red * g.blue * g.green) , 0)
+
+console.log(part2);
